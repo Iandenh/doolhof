91be4354f9422eb71d57be1936f5c63582d9ad5c
Ian den Hartog <iandenhartog@hotmail.com>
2016-06-17 22:15:59 +0200

    fix test

--- test/doolhof/VeldTest.java
+++ test/doolhof/VeldTest.java
@@ -5,7 +5,12 @@
  */
 package doolhof;
 
+import doolhof.Item.Finish;
+import doolhof.Item.Muur;
 import doolhof.Item.SpelItem;
+import doolhof.Item.Speler;
+import doolhof.level.Level;
+import doolhof.level.Level1;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import org.junit.After;
@@ -50,6 +55,30 @@ public class VeldTest {
         int expResult = 60;
         int result = instance.getxPos();
         assertEquals(expResult, result);
+    }
+    
+    @org.junit.Test
+    public void testPathFinish() {
+        Level level = new Level1();
+        Basisspel spel = new Basisspel();
+        spel.BouwSpelveld(level.getLevel());
+        ArrayList<Veld> velden = spel.getHelperPath();
+        
+        assertTrue((velden.get(velden.size()-1).getSpelItem() instanceof Finish));
+    }
+    
+    @org.junit.Test
+    public void testPathgeenMuur() {
+        Level level = new Level1();
+        Basisspel spel = new Basisspel();
+        spel.BouwSpelveld(level.getLevel());
+        ArrayList<Veld> velden = spel.getHelperPath();
+        for(Veld veld: velden){
+            if(veld.getSpelItem()  instanceof Muur){
+                fail("Zit muur in Path");
+            }
+        }
+        assertTrue(true);
     }
 
     
